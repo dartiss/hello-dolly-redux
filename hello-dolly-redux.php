85174f29dabe698c2198cc31b60fd187610534f3
David Artiss <david.artiss@automattic.com>
2023-05-20 09:15:18 +0100

    Initial release

---
+++ hello-dolly-redux.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * Hello Dolly Redux
+ *
+ * @package           Hello_Dolly
+ * @author            Matt Mullenweg
+ * @license           GPL-2.0-or-later
+ *
+ * Plugin Name:       Hello Dolly Redux
+ * Plugin URI:        https://wordpress.org/plugins/hello-dolly/
+ * Description:       This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
+ * Version:           0.1
+ * Requires at least: x.x
+ * Requires PHP:      x.x
+ * Author:            Matt Mullenweg
+ * Author URI:        http://ma.tt/
+ * Text Domain:       hello-dolly
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License version 2, as published by the Free Software Foundation. You may NOT assume
+ * that you can use any other version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+function hello_dolly_get_lyric() {
+
+	// These are the lyrics to Hello Dolly.
+
+	$lyrics = __(
+		"Hello, Dolly
+Well, hello, Dolly
+It's so nice to have you back where you belong
+You're lookin' swell, Dolly
+I can tell, Dolly
+You're still glowin', you're still crowin'
+You're still goin' strong
+I feel the room swayin'
+While the band's playin'
+One of our old favorite songs from way back when
+So, take her wrap, fellas
+Dolly, never go away again
+Hello, Dolly
+Well, hello, Dolly
+It's so nice to have you back where you belong
+You're lookin' swell, Dolly
+I can tell, Dolly
+You're still glowin', you're still crowin'
+You're still goin' strong
+I feel the room swayin'
+While the band's playin'
+One of our old favorite songs from way back when
+So, golly, gee, fellas
+Have a little faith in me, fellas
+Dolly, never go away
+Promise, you'll never go away
+Dolly'll never go away again",
+		'hello-dolly'
+	);
+
+	// Here we split the above string into individual lines, placing the result into an array.
+
+	$lyrics = explode( "\n", $lyrics );
+
+	// We randomly choose a line to display.
+
+	return wptexturize( $lyrics[ wp_rand( 0, count( $lyrics ) - 1 ) ] );
+}
+
+/**
+ * Display lyircs
+ *
+ * This just echoes the chosen line, we'll position it later.
+ */
+function hello_dolly() {
+
+	// Get a random line of the lyric.
+
+	$chosen = hello_dolly_get_lyric();
+
+	// Get the current user's locale.
+
+	$lang = '';
+	if ( 'en_' !== substr( get_user_locale(), 0, 3 ) ) {
+		$lang = ' lang="en"';
+	}
+
+	// Now, output the lyrics, with an appropriate HTML wrapper.
+
+	printf(
+		'<p id="dolly"><span class="screen-reader-text">%s </span><span dir="ltr"%s>%s</span></p>',
+		esc_html__( 'Quote from Hello Dolly song, by Jerry Herman:', 'hello-dolly' ),
+		esc_html( $lang ),
+		esc_html( $chosen )
+	);
+}
+
+// Now we set that function up to execute when the admin_notices action is called.
+add_action( 'admin_notices', 'hello_dolly' );
+
+/**
+ * Enqueue CSS
+ *
+ * This makes use of the enqueue functions to load the CSS file.
+ * We're loading the -min version of the file which is compressed. The non-compressed version of the file is also
+ * in the plugin folder, which is more "human readable".
+ */
+function dolly_css() {
+	wp_register_style( 'hello_dolly_css', plugin_dir_path( __FILE__ ) . 'hello-dolly-min.css', false, '1.0.0' );
+	wp_enqueue_style( 'hello_dolly_css' );
+}
+
+// This actions specifies the above function to be called only when loading scripts in admin (not the site's front-end).
+
+add_action( 'admin_enqueue_scripts', 'dolly_css' );
---
+++ readme.txt
@@ -0,0 +1,62 @@
+=== Hello Dolly Redux ===
+Contributors: matt, wordpressdotorg, dartiss
+Tags: market, coverage, share, w3tech, cms
+Requires at least: 4.6
+Tested up to: 6.2
+Requires PHP: 7.4
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong.
+
+== Description ==
+
+This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
+
+Thanks to Sanjib Ahmad for the artwork.
+
+== Installation ==
+
+World Domination can be found and installed via the Plugin menu within WordPress administration (Plugins -> Add New). Alternatively, it can be downloaded from WordPress.org and installed manually...
+
+1. Upload the entire `world-domination` folder to your `wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress administration.
+
+Voila! It's ready to go.
+
+== Frequently Asked Questions ==
+
+= Hey! My dashboard figure is different to the one shown on W3Techs =
+
+For performance reasons I cache the dashboard information for one week so, if the market figure changes in the meantime, you will get this discrepancy. If you find this keeps happening, please let me know and I’ll look at refreshing the cache more regularly.
+
+= I don't like the graphic on the dashboard that shows the percentage. Can I switch it off? =
+
+Yes you can! Head to Settings -> General in WP Admin and find the tickbox for this.
+
+= The dashboard graphic keeps changing color! =
+
+One of the parameters to generate the image is a hash - I pass a different one into it based on the current date, so will change daily.
+
+== Screenshots ==
+
+1. An example of Hello Dolly lyrics on the WordPress dashboard.
+
+== Changelog ==
+
+= 0.1 =
+* Added new meta header to the main plugin file.
+* Removed the in-line CSS and enqueued it as an external file.
+* Created both a minimised (31.25% reduction in size) and non-minimised version of the CSS (the former is used by the plugin).
+* Improved escaping of output.
+* The lyrics are now translatable.
+* Replaced with `mt_rand` with `wp_rand`, as that produces more purely randomised results.
+* Lots of additional comments in the code to better explain what's happening.
+* Compressed the image assets (non-lossy, using MozJPEG and JpegOptim), reducing them by 63.5%
+* Re-wrote this README, adding in additional content and making it a better example of what's required.
+
+== Upgrade Notice ==
+
+= 0.1 =
+* Initial release based on fork of Hello Dolly 1.7.2.
